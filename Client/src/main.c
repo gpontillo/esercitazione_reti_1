25db8e88a1317c0e461be9fb670ead30ab5f1733
Giovanni Pontillo <g.pontillo1@studenti.uniba.it>
2022-11-09 16:56:24 +0100

    Add portable code for sockets

--- Client/src/main.c
+++ Client/src/main.c
@@ -8,10 +8,38 @@
  ============================================================================
  */
 
+#if defined WIN32
+#include <winsock.h>
+#else
+#define closesocket close
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#endif
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	
+	#if defined WIN32
+	// Initialize Winsock
+	WSADATA wsaData;
+	int iResult =
+	WSAStartup(MAKEWORD(2,2),
+	&wsaData);
+	if (iResult != 0) {
+		printf("Error at WSAStartup()\n");
+		return 0;
+	}
+	#endif
+	int Mysocket;
+	
+	closesocket(Mysocket);
+	
+	#if defined WIN32
+	WSACleanup();
+	#endif
+	
 	return 0;
 }
--- Server/src/main.c
+++ Server/src/main.c
@@ -7,11 +7,39 @@
  Description : Hello World in C, Ansi-style
  ============================================================================
  */
+ 
+#if defined WIN32
+#include <winsock.h>
+#else
+#define closesocket close
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#endif
 
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	
+	#if defined WIN32
+	// Initialize Winsock
+	WSADATA wsaData;
+	int iResult =
+	WSAStartup(MAKEWORD(2,2),
+	&wsaData);
+	if (iResult != 0) {
+		printf("Error at WSAStartup()\n");
+		return 0;
+	}
+	#endif
+	int Mysocket;
+	
+	closesocket(Mysocket);
+	
+	#if defined WIN32
+	WSACleanup();
+	#endif
+	
 	return 0;
 }
